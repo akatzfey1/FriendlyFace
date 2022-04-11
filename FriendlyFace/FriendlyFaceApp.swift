981cdd7f72efd9c3ca5f48ae00962b30cadc58f3
akatzfey <katzfeyalex@gmail.com>
2022-04-10 18:24:50 -0700

    Initial Commit

---
+++ FriendlyFace/FriendlyFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendlyFaceApp.swift
+//  FriendlyFace
+//
+//  Created by Alexander Katzfey on 4/10/22.
+//
+
+import SwiftUI
+
+@main
+struct FriendlyFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
